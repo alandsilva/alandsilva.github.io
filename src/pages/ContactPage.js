fd9e3a07c11f6900a26d182cd0c64615deb5f448
Alan <dasilva.alan@outlook.com>
2022-01-24 08:48:45 +0200

    contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -4,9 +4,35 @@ import { FaLinkedin } from 'react-icons/fa';
 import styled from 'styled-components';
 import ContactInfoItem from '../components/ContactInfoItem';
 import SectionTitle from '../components/SectionTitle';
+import ContactForm from '../components/ContactForm';
 
 const ContactPageStyle = styled.div`
   padding: 10rem 0;
+  .contact__wrapper {
+    display: flex;
+    gap: 5rem;
+    margin-top: 7rem;
+    justify-content: space-between;
+    position: relative;
+  }
+
+  .left {
+    width: 100%;
+    max-width: 500px;
+  }
+  .right {
+    width: 100%;
+    max-width: 500px;
+  }
+  @media only screen and (max-width: 768px) {
+    .contact__wrapper  {
+      flex-direction: column;
+    }
+
+    .right {
+      padding: 4rem 2rem 2em 2rem;
+    }
+  }
 `;
 
 const ContactPage = () => {
@@ -20,13 +46,12 @@ const ContactPage = () => {
               icon={<MdMail />}
               text='dasilva.alan@outlook.com'
             />
-            <ContactInfoItem
-              icon={<FaLinkedin />}
-              text={'https://www.linkedin.com/in/alandsilva/'}
-            />
+            <ContactInfoItem icon={<FaLinkedin />} text={'alandsilva'} />
             <ContactInfoItem icon={<MdPhone />} text='+358 41 491 7610' />
           </div>
-          <div className='right'>contact form</div>
+          <div className='right'>
+            <ContactForm />
+          </div>
         </div>
       </div>
     </ContactPageStyle>
